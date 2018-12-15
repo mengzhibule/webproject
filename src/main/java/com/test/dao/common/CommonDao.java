424418e09f6474d9df304fb5813a2a1ba2a6e1bf
shao <15261052410@163.com>
2018-12-16 00:07:25 +0800

    优化dao,保证线程安全

--- src/main/java/com/test/dao/common/CommonDao.java
+++ src/main/java/com/test/dao/common/CommonDao.java
@@ -1,11 +1,24 @@
 package com.test.dao.common;
 
-import java.sql.*;
-import java.util.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class CommonDao {
     private Connection conn;
     private static CommonDao instance;
+
+    // 保存后，在同个线程中(同一次请求中)拿到的数据库连接都是同一个。
+    private static ThreadLocal<List<CommonDao>> daosLocal = new ThreadLocal<>();
 
     private CommonDao() {
         String url = "jdbc:mysql://192.168.129.128:3306/blog";
@@ -19,57 +32,61 @@ public class CommonDao {
         }
     }
 
-    public static  CommonDao getInstance(){
-        if (instance == null)
+    public static CommonDao getInstance() {
+        List<CommonDao> daos = daosLocal.get();
+        if (null == daos || daos.isEmpty()) {
             instance = new CommonDao();
-        return instance;
+            daos = new ArrayList<>();
+            daos.add(instance);
+            daosLocal.set(daos);
+            return instance;
+        } else {
+            return daos.get(0);
+        }
     }
 
-    public <T> List<T> findList(String sql,Class<T> cla){
+    public <T> List<T> findList(String sql, Class<T> cla) {
+        List<T> list = new ArrayList<>();
         PreparedStatement ps = null;
         ResultSet rs = null;
-        List<T> list = new ArrayList<>();
         try {
-            if(null != conn){
+            if (null != conn) {
                 ps = conn.prepareStatement(sql);
             }
             rs = ps.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 T obj = cla.newInstance();
-                HashMap<String,Object> rec = (HashMap<String,Object>)obj;
+                HashMap<String, Object> rec = (HashMap<String, Object>) obj;
                 ResultSetMetaData metaData = rs.getMetaData();
                 for (int i = 0; i < metaData.getColumnCount(); i++) {
-                    rec.put(metaData.getColumnName(i + 1).toLowerCase(),rs.getObject(metaData.getColumnName(i + 1)));
+                    rec.put(metaData.getColumnName(i + 1).toLowerCase(), rs.getObject(metaData.getColumnName(i + 1)));
                 }
                 list.add(obj);
             }
         } catch (SQLException | InstantiationException | IllegalAccessException e) {
             e.printStackTrace();
         } finally {
-            close(rs,ps);
+            close(ps, rs);
         }
         return list;
     }
 
-    public <T extends  HashMap<String,Object>> int insert(T cls) {
+    public <T extends HashMap<String, Object>> int insert(T obj) {
+        int rowCount = 0;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        int rowCount = 0;
         try {
-            HashMap<String,Object> rec = (HashMap<String,Object>)cls;
+            HashMap<String, Object> rec = (HashMap<String, Object>) obj;
             StringBuffer buffer = new StringBuffer();
             buffer.append("insert into ");
-            String packageName = cls.getClass().getPackage().getName();
-            String className = cls.getClass().getName();
-            String name = className.substring(packageName.length() + 1,className.length()).toLowerCase();
-            buffer.append(name);
+            buffer.append(getTableName(obj));
             buffer.append(" ( ");
             Set<String> keys = rec.keySet();
             int i = 0;
             for (String key : keys) {
                 buffer.append(key);
-                i ++;
-                if(i != keys.size()){
+                i++;
+                if (i != keys.size()) {
                     buffer.append(",");
                 }
             }
@@ -77,40 +94,29 @@ public class CommonDao {
             buffer.append(" values ( ");
             for (int j = 0; j < keys.size(); j++) {
                 buffer.append("?");
-                if(j != keys.size() - 1){
+                if (j != keys.size() - 1) {
                     buffer.append(",");
                 }
             }
             buffer.append(" ) ");
-            ps = conn.prepareStatement(buffer.toString());
-            int j = 0;
-            for (String key : keys) {
-                ps.setObject(++ j, rec.get(key));
-            }
-            System.out.println(buffer.toString());
-            conn.setAutoCommit(false);
-            rowCount = ps.executeUpdate();
-            conn.commit();
-        } catch (SQLException  e) {
+            rowCount = transcation(buffer.toString(), rec, false);
+        } catch (SQLException e) {
             e.printStackTrace();
-        }  finally {
-            close(rs,ps);
+        } finally {
+            close(ps, rs);
         }
         return rowCount;
     }
 
-    public <T extends HashMap<String,Object>> int update(T obj){
+    public <T extends HashMap<String, Object>> int update(T obj) {
+        int rowCount = 0;
         PreparedStatement ps = null;
         ResultSet rs = null;
-        int rowCount = 0;
         try {
-            HashMap<String,Object> rec = (HashMap<String,Object>)obj;
+            HashMap<String, Object> rec = (HashMap<String, Object>) obj;
             StringBuffer buffer = new StringBuffer();
             buffer.append("update ");
-            String packageName = obj.getClass().getPackage().getName();
-            String className = obj.getClass().getName();
-            String name = className.substring(packageName.length() + 1,className.length()).toLowerCase();
-            buffer.append(name);
+            buffer.append(getTableName(obj));
             buffer.append(" set ");
             Set<String> keys = rec.keySet();
             int i = 0;
@@ -118,43 +124,31 @@ public class CommonDao {
                 buffer.append(key);
                 buffer.append(" = ");
                 buffer.append(" ? ");
-                i ++;
-                if(i != keys.size()){
+                i++;
+                if (i != keys.size()) {
                     buffer.append(",");
                 }
             }
             // FIXME 读取注解，获取主键，暂时写死，后期修改
             buffer.append(" where id = ? ");
-            ps = conn.prepareStatement(buffer.toString());
-            int j = 0;
-            for (String key : keys) {
-                ps.setObject(++ j, rec.get(key));
-            }
-            ps.setObject(++ j,rec.get("id"));
-            System.out.println(buffer.toString());
-            conn.setAutoCommit(false);
-            rowCount = ps.executeUpdate();
-            conn.commit();
-        } catch (SQLException  e) {
+            rowCount = transcation(buffer.toString(), rec, true);
+        } catch (SQLException e) {
             e.printStackTrace();
-        }  finally {
-            close(rs,ps);
+        } finally {
+            close(ps, rs);
         }
         return rowCount;
     }
 
-    public <T extends HashMap<String,Object>> int delete(T obj){
+    public <T extends HashMap<String, Object>> int delete(T obj) {
         PreparedStatement ps = null;
         ResultSet rs = null;
         int rowCount = 0;
         try {
-            HashMap<String,Object> rec = (HashMap<String,Object>)obj;
+            HashMap<String, Object> rec = (HashMap<String, Object>) obj;
             StringBuffer buffer = new StringBuffer();
             buffer.append("delete from ");
-            String packageName = obj.getClass().getPackage().getName();
-            String className = obj.getClass().getName();
-            String name = className.substring(packageName.length() + 1,className.length()).toLowerCase();
-            buffer.append(name);
+            buffer.append(getTableName(obj));
             buffer.append(" where ");
             Set<String> keys = rec.keySet();
             int i = 0;
@@ -162,38 +156,92 @@ public class CommonDao {
                 buffer.append(key);
                 buffer.append(" = ");
                 buffer.append(" ? ");
-                i ++;
-                if(i != keys.size()){
+                i++;
+                if (i != keys.size()) {
                     buffer.append(" and ");
                 }
             }
-            ps = conn.prepareStatement(buffer.toString());
-            int j = 0;
-            for (String key : keys) {
-                ps.setObject(++ j, rec.get(key));
-            }
-            System.out.println(buffer.toString());
-            conn.setAutoCommit(false);
-            rowCount = ps.executeUpdate();
-            conn.commit();
-        } catch (SQLException  e) {
+            rowCount = transcation(buffer.toString(), rec, false);
+        } catch (SQLException e) {
             e.printStackTrace();
-        }  finally {
-            close(rs,ps);
+        } finally {
+            close(ps, rs);
         }
         return rowCount;
     }
 
-    public void close(ResultSet rs,PreparedStatement ps){
+    private int transcation(String sql, Map<String, Object> rec, boolean flag) throws SQLException {
+        PreparedStatement ps = null;
+        //ResultSet rs = null;
+        ps = conn.prepareStatement(sql);
+        int rowCount = 0;
+        int j = 0;
+        Set<String> keys = rec.keySet();
+        for (String key : keys) {
+            ps.setObject(++j, rec.get(key));
+        }
+        if (flag) {
+            ps.setObject(++j, rec.get("id"));
+        }
+        conn.setAutoCommit(false);
+        rowCount = ps.executeUpdate();
+        conn.commit();
+        return rowCount;
+    }
+
+    private <T extends HashMap<String, Object>> String getTableName(T obj) {
+        String packageName = obj.getClass().getPackage().getName();
+        String className = obj.getClass().getName();
+        String name = className.substring(packageName.length() + 1, className.length()).toLowerCase();
+        return name;
+    }
+
+    public void close(PreparedStatement ps, ResultSet rs) {
         try {
-            if(null != rs){
+            if (null != rs) {
                 rs.close();
             }
-            if(null != ps){
+            if (null != ps) {
                 ps.close();
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            if (null != conn) {
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            removeConnetion();
+        }
+    }
+
+    public void closeAllConns() {
+        List<CommonDao> daos = daosLocal.get();
+        if (null == daos) {
+            return;
+        }
+        for (int i = 0; i < daos.size(); i++) {
+            daos.get(i).close();
+        }
+        daos.clear();
+    }
+
+    private void removeConnetion() {
+        List<CommonDao> daos = daosLocal.get();
+        if (null != daos) {
+            for (int i = 0; i < daos.size(); i++) {
+                CommonDao dao = daos.get(i);
+                if (dao.conn == conn) {
+                    daos.remove(i);
+                }
+                i--;
+            }
         }
     }
 }
--- src/main/java/com/test/dao/common/test/TestDb.java
+++ src/main/java/com/test/dao/common/test/TestDb.java
@@ -6,26 +6,41 @@ import java.util.List;
 
 public class TestDb {
     public static void main(String[] args) {
-        CommonDao dao = CommonDao.getInstance();
-        List<Test> list = dao.findList("select * from test", Test.class);
-        for (Test test : list) {
-            System.out.println(test.getId());
-            System.out.println(test.getUsername());
-            System.out.println(test.getPassword());
-        }
-        Test test = new Test();
-        test.setId("3");
-        test.setUsername("4444");
-        test.setPassword("3");
-        int insert = dao.delete(test);
-        System.out.println(insert);
+        for(int i = 0; i < 500; i ++){
+            Runnable fun = new Runnable(){
+                @Override
+                public void run() {
+                    CommonDao dao = CommonDao.getInstance();
+                    List<Test> list = dao.findList("select * from test", Test.class);
+                    for (Test test : list) {
+                        System.out.println(test.getId());
+                        System.out.println(test.getUsername());
+                        System.out.println(test.getPassword());
+                    }
+                    Test test = new Test();
+                    test.setId("4");
+                    test.setUsername("66");
+                    test.setPassword("3");
+                    int insert = dao.delete(test);
+                    System.out.println(insert);
+                    list = dao.findList("select * from test", Test.class);
+                    for (Test rec : list) {
+                        System.out.println(rec.getId());
+                        System.out.println(rec.getUsername());
+                        System.out.println(rec.getPassword());
+                    }
+                }
+            };
+            Thread thread = new Thread(fun);
+            thread.start();
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-        list = dao.findList("select * from test", Test.class);
-        for (Test rec : list) {
-            System.out.println(rec.getId());
-            System.out.println(rec.getUsername());
-            System.out.println(rec.getPassword());
         }
+
     }
 
 
